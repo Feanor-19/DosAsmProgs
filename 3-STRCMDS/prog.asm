84342bf59446cd620d576d23d8368fd25a08d175
Filipp <sap.phil.v@mail.ru>
2024-02-13 21:27:45 +0300

    3 - мелкая правка в strlen

--- 3-STRCMDS/prog.asm
+++ 3-STRCMDS/prog.asm
@@ -8,15 +8,14 @@ Start:
             pop es
 
             mov di, offset X
-            ;call Strlen
+            call Strlen
 
-            mov ax, 0B9A2h
             call PrnW
 
             mov ax, 4c13h
             int 21h
 
-X db 'str$'
+X db 'test$'
 
 ;-------------------------------------------
 ; Strlen
@@ -43,55 +42,12 @@ STRLEN_CX_DEF_VAL equ 0FFFFh
 
             mov ax, STRLEN_CX_DEF_VAL
             sub ax, cx
+            dec ax
 
             ret
             endp
 ;-------------------------------------------
 
-;-------------------------------------------
-; PrnW
-; Description:
-;   Prints to standart output AX
-;   in hex (using int 21h (02h))
-; DESTROYS:
-;   CX, DX
-;-------------------------------------------
-PrnW        proc
-
-            mov si, 3h          ; starting from smallest bytes,
-                                ; which are in the rightmost character
-
-            ; loop start
-            mov cx, 4h          ; rotating ax 4 times
-PrnWL:      mov dl, al
-            and dl, 00001111b
-            cmp dl, 10d
-            jge PrnWLtr
-
-            add dl, '0' ; '0' ... '9'
-            jmp short PrnWFin
-
-PrnWLtr:    add dl, 'A' ; 'A' ... 'F'
-            sub dl, 0Ah
-
-PrnWFin:    mov byte ptr [PrnWData + si], dl
-            ror ax, 4
-            dec si
-            loop PrnWL
-            ; loop end
-
-            ; printing
-            push ax                 ; saving ax
-            mov dx, offset PRNWDATA
-            mov ah, 09h
-            int 21h
-            pop ax                  ; ressurecting ax
-
-            ; end
-            ret
-            endp
-
-PrnWData    db ?, ?, ?, ?, '$'
-;-------------------------------------------
+include prnw.asm
 
 end         Start
